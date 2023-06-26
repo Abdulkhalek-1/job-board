4292cb83a640e77328477bf70a9e9e02da4523c3
High-2005 <highgamer2005@gmail.com>
2023-06-26 14:22:11 -0400

    apply_in_job

--- job/models.py
+++ job/models.py
@@ -32,3 +32,16 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Apply(models.Model):
+    job = models.ForeignKey(job, related_name="apply_job", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    website = models.URLField()
+    cv = models.FileField(upload_to='apply/')
+    cover_letter = models.TextField()
+    created_at = models.TimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
--- job/views.py
+++ job/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import job
+from .forms import ApplyForm
 
 
 def job_list(request):
@@ -8,16 +9,23 @@ def job_list(request):
     paginator = Paginator(job_list, 10)
     page_number = request.GET.get('p')
     page_obj = paginator.get_page(page_number)
-    context = {"jobs": page_obj,
-                "count": job_list.count
-                }
+    context = {"jobs": page_obj, "count": job_list.count}
     return render(request, "job/job_list.html", context)
 
 
 def job_detail(request, id):
     try:
         job_detail = job.objects.get(id=id)
-        context = {"job": job_detail}
+        if request.method == "POST":
+            form = ApplyForm(request.POST, request.FILES)
+            if form.is_valid():
+                myform = form.save(commit=False)
+                myform.job = job_detail
+                myform.save()
+        else:
+            form = ApplyForm()
+
+        context = {"job": job_detail, "form": form}
         return render(request, "job/job_detail.html", context)
     except:
         return render(request, "job/job_detail.html", {"job.is_active": False})
